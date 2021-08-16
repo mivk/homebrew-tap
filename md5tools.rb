2dae394f58b78a9020ee89c5c163caa6ffe70691
mivk <mivk@users.noreply.github.com>
2021-08-16 18:42:14 +0200

    Update md5tools.rb

--- md5tools.rb
+++ md5tools.rb
@@ -4,7 +4,7 @@ class Md5tools < Formula
   url "https://github.com/mivk/md5tools/archive/refs/tags/v0.1.7.tar.gz"
   #sha256 :no_check
   #sha256 "cddb8810d41390f464025d5323e7e8ee54cb1801c67937c08382415bfb5a1209"
-  version "0.1.6"
+  version "0.1.7"
   #revision 1
   #version :latest
 
