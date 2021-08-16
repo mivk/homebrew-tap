105fc2eaca9f4a672ca0c000d5be5c7af428bc6e
mivk <mi.gh@alma.ch>
2021-08-16 11:14:17 +0200

    add md5tools

---
+++ md5tools.rb
@@ -0,0 +1,11 @@
+class md5tools < Formula
+  desc "Tools for md5 checksums files"
+  homepage "https://github.com/mivk/md5tools"
+  head "https://github.com/mivk/md5tools"
+  version "0.1.5"
+
+  def install
+    bin.install "md5dirs"
+  end
+
+end
