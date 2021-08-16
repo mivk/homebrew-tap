f9055677c24d5706e8dd60c94b76def5aeb05638
mivk <mi.gh@alma.ch>
2021-08-16 17:49:10 +0200

    md5tools: try with version :latest

--- md5tools.rb
+++ md5tools.rb
@@ -1,13 +1,11 @@
 class Md5tools < Formula
   desc "Tools for md5 checksums files"
   homepage "https://github.com/mivk/md5tools"
-  url "https://github.com/mivk/md5tools/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "cddb8810d41390f464025d5323e7e8ee54cb1801c67937c08382415bfb5a1209"
-  version "0.1.5"
-  revision 1
-
+  version :latest
+  sha256 :no_check
   def install
     bin.install "md5dirs"
   end
 
 end
+
