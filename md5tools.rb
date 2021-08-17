d88787c077fb5a5a74146f7faa5133b8d0cdc0cf
mivk <mi.gh@alma.ch>
2021-08-17 16:39:13 +0200

    md5tools: use only head, without version

--- md5tools.rb
+++ md5tools.rb
@@ -1,8 +1,8 @@
 class Md5tools < Formula
   desc "Tools for md5 checksums files"
   homepage "https://github.com/mivk/md5tools"
-  url "https://github.com/mivk/md5tools/archive/refs/heads/main.zip"
-  version "0.1.9"
+  head "https://github.com/mivk/md5tools.git"
+  #version "0.1.9"
 
   def install
     bin.install "md5dirs"
