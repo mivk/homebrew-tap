c6bebed91e87e905a5254576405df3e2b8d22e74
mivk <mi.gh@alma.ch>
2021-08-17 09:51:17 +0200

    md5tools: try head branch main and no sha256

--- md5tools.rb
+++ md5tools.rb
@@ -1,16 +1,10 @@
 class Md5tools < Formula
   desc "Tools for md5 checksums files"
   homepage "https://github.com/mivk/md5tools"
-  url "https://github.com/mivk/md5tools/archive/refs/tags/v0.1.6.tar.gz"
-  #sha256 :no_check
-  #sha256 "cddb8810d41390f464025d5323e7e8ee54cb1801c67937c08382415bfb5a1209"
-  version "0.1.6"
-  #revision 1
-  #version :latest
+  head "https://github.com/mivk/md5tools.git", branch "main"
+  version "0.1.8"
 
   def install
     bin.install "md5dirs"
   end
-
 end
-
