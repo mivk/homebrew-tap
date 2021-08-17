bfc76394bd5776db06843625c957ae55119f5965
mivk <mi.gh@alma.ch>
2021-08-17 10:08:11 +0200

    md5tools: try url main

--- md5tools.rb
+++ md5tools.rb
@@ -1,7 +1,7 @@
 class Md5tools < Formula
   desc "Tools for md5 checksums files"
   homepage "https://github.com/mivk/md5tools"
-  head "https://github.com/mivk/md5tools.git", branch "main"
+  url "https://github.com/mivk/md5tools/archive/refs/heads/main.zip"
   version "0.1.8"
 
   def install
