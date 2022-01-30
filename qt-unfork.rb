1bf61dca42d55a0784766a6c19c5257e95294099
mivk <mivk@users.noreply.github.com>
2022-01-30 20:32:44 +0100

    Update qt-unfork.rb
    New v. 0.1.1 with better QT 7 detection

--- qt-unfork.rb
+++ qt-unfork.rb
@@ -1,9 +1,9 @@
 class QtUnfork < Formula
   desc "Flatten old Quicktime movies which use a separate resource fork for the moov atom"
   homepage "https://github.com/mivk/qt-unfork"
-  url "https://github.com/mivk/qt-unfork/archive/v0.0.1.tar.gz"
-  sha256 "0b080cd67495ea40d226e024dd7650190406ff4b97cc4c86220c2514b708ce34"
-  version "0.0.1"
+  url "https://github.com/mivk/qt-unfork/archive/0.1.1.tar.gz"
+  sha256 "72dd347e44d7f7b1bf9f4647548923f4fa14b66464b90f820c09f38336038107"
+  version "0.1.1"
 
   def install
     bin.install "qt-unfork"
